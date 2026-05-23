d1866c692bb22a60633e194c80fb2d7ea291c455
syedanazish <fatimasyedanazish14@gmail.com>
2026-05-23 13:48:42 +0500

    WIP

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,6 +1,16 @@
-﻿namespace RailwayCateringERPSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RailwayCateringERPSystem.Models
 {
     public class Order
     {
+        public Guid OrderId { get; set; } = Guid.NewGuid();
+        public string CoachNumber { get; set; }
+        public string SeatNumber { get; set; }
+        public string Status { get; set; } = "Pending";
+
+        public decimal TotalAmount { get; set; } = 0;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
--- Models/Train.cs
+++ Models/Train.cs
@@ -1,6 +1,13 @@
-﻿namespace RailwayCateringERPSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RailwayCateringERPSystem.Models
 {
     public class Train
     {
+        public Guid TrainId { get; set; } = Guid.NewGuid();
+        public string TrainName { get; set; }
+        public string TrainNumber { get; set; }
+        public string Route { get; set; }
+        public int TotalCoaches { get; set; }
     }
 }
