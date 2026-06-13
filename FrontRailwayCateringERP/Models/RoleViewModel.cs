f08b34d90e11da3b337b9edec8e2953223ffba84
Sameer Aslam <imsameer1328@gmail.com>
2026-06-14 00:14:58 +0500

    Remove obj and launchSettings from tracking

--- FrontRailwayCateringERP/Models/RoleViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FrontRailwayCateringERP.Models
-{
-    public class RoleViewModel
-    {
-        public Guid RoleId { get; set; }
-        public string Name { get; set; }
-        public string Permissions { get; set; }
-    }
-}
